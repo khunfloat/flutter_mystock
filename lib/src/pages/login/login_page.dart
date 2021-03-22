dd4dc09bbf7ded0956ac8b6ea5b96deeb59538e0
Chayoot Kosiwanich <60844731+khunfloat@users.noreply.github.com>
2021-03-22 13:51:02 +0700

    background colot gradient

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -10,6 +7,20 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         fit: StackFit.expand,
         children: [
+          Container(
+            // color: Colors.amber,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0XFFf12711),
+                  Color(0XFFf5af19),
+                ],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                stops: [0.0, 1.0],
+              ),
+            ),
+          ),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
