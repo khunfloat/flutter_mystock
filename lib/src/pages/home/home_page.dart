40f04aa6fdf3ccd88db84c45039a5dc0db2ad7af
Chayoot Kosiwanich <60844731+khunfloat@users.noreply.github.com>
2021-03-25 10:16:46 +0700

    ios config

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -23,6 +23,7 @@ class _HomePageState extends State<HomePage> {
       drawer: CommonDrawer(),
       appBar: AppBar(
         title: Text("PRODUCT LIST"),
+        centerTitle: false,
       ),
       body: FutureBuilder<List<ProductResponse>>(
         future: NetworkService().productAll(),
